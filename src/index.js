e1b050ad5621e26891291b43b724bea0749ca0c7
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-08-03 21:50:26 +0300

    feat: add folder components

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-	return (
-		<ul>
-			<li>Learn React</li>
-			<li>Build Awesome App</li>
-		</ul>
-	);
-};
-
-const AppHeader = () => {
-	return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-	return <input placeholder="search" />;
-};
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list'
 
 const App = () => {
+
+	const todoData = [
+		{ label: 'Drink Coffee', important: false, id: 1 },
+		{ label: 'Make Awesome App', important: true, id: 2 },
+		{ label: 'Have a lunch', important: false, id: 3 },
+
+	];
+
 	return (
 		<div>
 			<AppHeader />
 			<SearchPanel />
-			<TodoList />
+			<TodoList todos={todoData} />
 		</div>
 	);
 };
